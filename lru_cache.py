69d3165963fe7068b12afc2dfab3a0b722436b64
justinjoco <justin.joco@gmail.com>
2019-06-10 11:51:39 -0700

    reverse linked list

--- lru_cache.py
+++ lru_cache.py
@@ -22,29 +22,30 @@ cache.put(4, 4);    // evicts key 1
 cache.get(1);       // returns -1 (not found)
 cache.get(3);       // returns 3
 cache.get(4);       // returns 4
+
+https://leetcode.com/problems/lru-cache/solution/
+
+Use two data structures:
+* Dictionary: for O(1) getting and pushing
+* Linked list: to keep track of the least and most recently used caches 
+
 '''
-from collections import OrderedDict
-class LRUCache (OrderedDict):
+
+
+
+class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
+        
 
         
         
     def get(self, key: int) -> int:
-        if key not in self:
-            return -1
-        self.move_to_end(key)
-        return self[key]
-            
+
 
     def put(self, key: int, value: int) -> None:   
         
-        if key in self:
-            self.move_to_end(key)
-        self[key] = value
-        if len(self) > self.capacity:
-            self.popitem(last = False)
+
             
         
         
